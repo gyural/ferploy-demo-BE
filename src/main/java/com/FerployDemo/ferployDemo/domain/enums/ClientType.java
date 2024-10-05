a8f90e58b91f427f61c7d32edd0761b048664aeb
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2024-10-06 07:40:23 +0900

    fix:clientType-kor

--- src/main/java/com/FerployDemo/ferployDemo/domain/enums/ClientType.java
+++ src/main/java/com/FerployDemo/ferployDemo/domain/enums/ClientType.java
@@ -6,12 +6,12 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ClientType {
-    PARTNER("파트너사"),
-    STARTUP("창업기업"),
-    INVESTOR("투자사"),
-    INSTITUTION("기관"),
-    CUSTOMER("고객"),
-    OTHERS("기타");
+    파트너사("파트너사"),
+    창업기업("창업기업"),
+    투자사("투자사"),
+    기관("기관"),
+    고객("고객"),
+    기타("기타");
 
     private final String description;
 }
